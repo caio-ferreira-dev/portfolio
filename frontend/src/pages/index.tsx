ae5e834399dbab72e40c676e36e7e64e8be44578
caio-ferreira-dev <caioferreirasdj@gmail.com>
2023-09-06 15:14:13 -0300

    criação da estrutura da home page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head'
 import styles from '@/styles/pages/home.module.css'
 import NavButtons from '../../components/NavButtons'
@@ -10,10 +11,14 @@ export default function Home() {
         <title>Caio Ferreira</title>
         <meta name="description" content="Portfolio de Caio Ferreira" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
       </Head>
       <main className={styles.main}>
+        <h2>Caio Ferreira</h2>
+        <h1>Desenvolvedor JavaScript.</h1>
+        <p>Apaixonado por criar aplicativos web que integrem perfeitamente back-ends robustos com front-ends elegantes.</p>
         <NavButtons buttonsAmount={5} selectedPage={1}/>
+        <img src="/profile.png" alt="Foto de Caio Ferreira"/>
       </main>
     </>
   )
